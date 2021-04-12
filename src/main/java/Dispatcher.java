826fc97846a6ab8e0c25678de3b8259c2c91da66
matcala <matteo.calabrese@studenti.unimi.it>
2021-04-12 15:40:57 +0200

    Added missing class

---
+++ src/main/java/Dispatcher.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Dispatcher {
+
+    public static void main(String[] args) throws Exception {
+
+        Socket connection = null;
+        ServerSocket welcomeSocket = null;
+        Reservation reservation;
+        welcomeSocket = new ServerSocket(6789);             //quante cazzo di chiamate accetti?!
+        reservation = new Reservation();
+
+        while (true) {
+            System.out.println("\n[+] Waiting for connection");
+            connection = welcomeSocket.accept();
+            System.out.println("[+] Connected with: " + connection.getInetAddress() + ":" + connection.getPort());
+
+            // Per testare l'acquisizione e il mantenimento del lock su Reservation
+            //WorkerRitardato newRitardato = new WorkerRitardato(connection, reservation);
+            Worker newWorker = new Worker(connection, reservation);
+
+            //newRitardato.start();
+            newWorker.start();
+        }
+
+    }
+}
